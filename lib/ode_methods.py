b810849a8ef03d5f9fa921cc644d7e1747d03582
Rodrigue T <tavernier.rodrigue@gmail.com>
2024-02-17 12:25:32 +0100

    Improved performances
    I noticed I was relying on sympy too much. I used lambdify too often when I only needed to do it once. Helped improved compute time by a huge factor.

--- lib/ode_methods.py
+++ lib/ode_methods.py
@@ -137,6 +137,7 @@ class CrankNicolson(Scheme):
         
     def _next_value(self, t_n, y_n, h):
         tmp = self._ode._mainflam
+        
         def _to_solve(x):
             arg = x.detach().numpy()
             return x - torch.from_numpy(y_n) - (h/2) * (
@@ -206,6 +207,9 @@ class RungeKutta(Scheme):
             
         self._explicit = _RKCheck(self._butcher[0])
         self._q = len(self._butcher[0])
+        
+    def _next_value(self, t_n, y_n, h):
+        return self._next_value_explicit(t_n, y_n, h) if self._explicit else self._next_value_implicit(t_n, y_n, h)
     
     def _next_value_explicit(self, t_n, y_n, h):
         q = self._q
@@ -227,102 +231,29 @@ class RungeKutta(Scheme):
     def _next_value_implicit(self, t_n, y_n, h):
         ode = self._ode
         q = self._q
-        t = ode._str2symb["t"]
+        flam = ode._mainflam
         
-        to_solve = np.array([])
+        def _to_solve(x):
+            arg = x.detach().numpy()
+            yi = np.array([])
+            for i in range(q):
+                tmp = copy.deepcopy(y_n)
+                for j in range(q):
+                    start = ode._dim * ode._order * j
+                    end = ode._dim * ode._order * (j + 1)
+                    tmp += self._butcher[0][i][j] * h * np.array(flam(t_n + self._butcher[2][j] * h, *arg[start:end]))
+                        
+                yi = np.append(yi, tmp)
+            return x - torch.from_numpy(yi)
         
+        v0 = np.array([])
         for i in range(q):
-            tmp1 = np.array(self._tmp_Ysymbols[i]) - copy.deepcopy(y_n)
-            
-            for j in range(q):
-                tmp2 = np.array(self._tmp_Ysymbols[j])
-                tmp2 = tmp2[ode._dim:]
-                
-                for k in range(1, ode._dim+1):
-                    expr = self._tmp_expr[i][k].subs(t, t_n + self._butcher[2][j] * h)
-                    tmp2 = np.append(tmp2, expr)
-                    
-                aij = self._butcher[0][i][j]
-                tmp1 -= h * aij * tmp2
-            
-            to_solve = np.append(to_solve, tmp1)
-            
-        # Newton
-        
-        y_symbols = np.array([])
-        x0 = np.array([])
-        for i in range(q):
-            y_symbols = np.append(y_symbols, self._tmp_Ysymbols[i])
-            x0 = np.append(x0, copy.deepcopy(y_n))
-            
-        V = sp.Matrix(to_solve)
-        J = sp.Matrix.jacobian(V, y_symbols)
-        solved = newton_n(V, J, x0, y_symbols)
-        
-        # Use intermediate values for y(n+1)
+            v0 = np.append(v0, y_n)
+        yi_val = _newton(_to_solve, v0)
         
         res = copy.deepcopy(y_n)
-        symbols = [ode._str2symb[s] for s in ode._str2symb]
         for i in range(q):
-            bi = self._butcher[1][i]
-            ti = t_n + self._butcher[2][i] * h
-            step = ode._dim * ode._order
-            tmp = bi * np.append(solved[i*step+ode._dim:i*step+step], [sp.lambdify(symbols, ode._expr[j])(ti, *solved[i*step:i*step+step]) for j in range(1, ode._dim+1)])
-            res += h * tmp
-            
+            start = ode._dim * ode._order * i
+            end = ode._dim * ode._order * (i + 1)
+            res += self._butcher[1][i] * h * np.array(flam(t_n + self._butcher[2][i]*h, *yi_val[start:end]))
         return res
-                 
-    def _solve_explicit(self, T, N):
-        ode = self._ode
-        n = ode._dim
-        h: float = T / N
-        time = np.array([ode._t0 + i * h for i in range(N)])
-        value = np.empty((N, n * ode._order))
-        value[0] = np.array(ode._initialValues)
-        for i in range(1, N):
-            print(f"step {i}")
-            v = self._next_value_explicit(time[i-1], value[i-1], h)
-            value[i] = v
-        res = np.array([v[:ode._dim] for v in value])
-        return (time, res)
-    
-    def _solve_implicit(self, T, N):
-        ode = self._ode
-        n = ode._dim
-        h: float = T / N
-        time = np.array([ode._t0 + i * h for i in range(N)])
-        value = np.zeros((N, n * ode._order))
-        value[0] = ode._initialValues
-        for i in range(1, N):
-            v = self._next_value_implicit(time[i-1], value[i-1], h)
-            value[i] = v
-        res = np.array([v[:ode._dim] for v in value])
-        return (time, res)
-    
-    
-    def _initialiaze_implicit(self):
-        self._tmp_Ysymbols = {i: [] for i in range(self._q)}
-        for i in range(self._q):
-            for d in range(self._ode._order):
-                for j in range(1, self._ode._dim+1):
-                    nam = "d" * d + self._ode._symb + f"{i}_{j}"
-                    symb = sp.symbols(nam)
-                    self._tmp_Ysymbols[i].append(symb)
-                          
-        self._tmp_expr = {i: {} for i in range(self._q)}
-        symbols = [self._ode._str2symb[s] for s in self._ode._str2symb if s != "t"]
-        for k in range(self._q):
-            for i in range(1, self._ode._dim+1):
-                expri = self._ode._expr[i]
-                for j in range(self._ode._dim * self._ode._order):
-                    expri = expri.subs(symbols[j], self._tmp_Ysymbols[k][j])
-                self._tmp_expr[k][i] = expri
-    
-    def solve(self, T, N):
-        if self._explicit:
-            return self._solve_explicit(T, N)
-        else:
-            self._initialiaze_implicit()
-            return self._solve_implicit(T, N)
-            
-        
